00245275f641835fdbbeb0fa3506bad297a27f15
optimisticside <47266822+optimisticside@users.noreply.github.com>
2022-08-02 13:17:18 -0700

    Add switch-statement parser

--- src/parse.c
+++ src/parse.c
@@ -35,112 +35,6 @@ static struct token *expect(struct parser *parser, int kind) {
 }
 
 /*
- * Parse an if statement with a possible else case.
- *
- * if-statement:
- *   if ( expression ) statement
- *   if ( expression ) statement else statement
- */
-static struct tree *ifstmt(struct parser *parser) {
-	struct tree *cond;
-	struct tree *thenbody, *elsebody;
-
-	expect(parser, T_IF);
-	expect(parser, T_LPAREN);
-	cond = expr(parser);
-	expect(parser, T_RPAREN);
-
-	thenbody = stmt(parser);
-	if (accept(parser, T_ELSE))
-		elsebody = stmt(parser);
-	return mkastnode(AST_IFSTMT, cond, thenbody, elsebody);
-}
-
-/*
- * Parse a while statement.
- *
- * while-statement:
- *   while ( expression ) statement
- */
-static struct tree *whilestmt(struct parser *parser) {
-	struct tree *cond, *body;
-
-	expect(parser, T_WHILE);
-	expect(parser, T_LPAREN);
-	cond = expr(parser);
-	expect(parser, T_RPAREN);
-
-	body = stmt(parser);
-	return mkastbinary(AST_WHILESTMT, cond, body);
-}
-
-/*
- * Parse a do statement.
- *
- * do-statement:
- *   do statement while ( expression ) ;
- */
-static struct tree *dostmt(struct parser *parser) {
-	struct tree *body, *cond;
-
-	expect(parser, T_DO);
-	body = stmt(parser);
-
-	expect(parser, T_WHILE);
-	expect(parser, T_LPAREN);
-	cond = expr(parser);
-	expect(parser, T_RPAREN);
-	expect(parser, T_SEMI);
-
-	return mkastbinary(AST_DOSTMT, cond, body);
-}
-
-/*
- * Parse an iteration satement.
- *
- * iteration-statement:
- *   while ( expression ) statement
- *   do statement while ( expression ) ;
- *   for ( expression-statement expression-statement ) statement
- *   for ( expression-statement expression-statement expression-statement ) statement
- *   for ( declaration expression-statement ) statement
- *   for ( declaration expression-statement expression-statement ) statement
- */
-static struct tree *iterstmt(struct parser *parser) {
-	switch (parser->token->kind) {
-	case T_WHILE:
-		return whilestmt(parser);
-	case T_DO:
-		return dostmt(parser);
-	case T_FOR:
-		return forstmt(parser);
-	}
-}
-
-/*
- * Parse a jump statement.
- *
- * jump-statement:
- *   goto identifier ;
- *   continue ;
- *   break ;
- *   return ;
- *   return expression ;
- */
-static struct tree *jumpstmt(struct parser *parser) {
-	switch (parser->token->kind) {
-	case T_GOTO:
-		return gotostmt(parser);
-	case T_CONTINUE:
-		return contstmt(parser);
-	case T_BREAK:
-		return breakstmt(parser);
-	case T_RETURN:
-		return returnstmt(parser);
-	}
-}
-
-/*
  * Parse a direct-declarator.
  *
  * direct-declarator:
@@ -202,3 +96,146 @@ static struct declarator *declarator(struct parser *parser) {
 static struct tree *declaration(struct parser *parser) {
 
 }
+
+/*
+ * Parse an if statement with a possible else case.
+ *
+ * if-statement:
+ *   if ( expression ) statement
+ *   if ( expression ) statement else statement
+ */
+static struct tree *ifstmt(struct parser *parser) {
+	struct tree *cond;
+	struct tree *thenbody, *elsebody;
+
+	expect(parser, T_IF);
+	expect(parser, T_LPAREN);
+	cond = expr(parser);
+	expect(parser, T_RPAREN);
+
+	thenbody = stmt(parser);
+	if (accept(parser, T_ELSE))
+		elsebody = stmt(parser);
+	return mkastnode(AST_IFSTMT, cond, thenbody, elsebody);
+}
+
+/*
+ * Parse a while statement.
+ *
+ * while-statement:
+ *   while ( expression ) statement
+ */
+static struct tree *whilestmt(struct parser *parser) {
+	struct tree *cond, *body;
+
+	expect(parser, T_WHILE);
+	expect(parser, T_LPAREN);
+	cond = expr(parser);
+	expect(parser, T_RPAREN);
+
+	body = stmt(parser);
+	return mkastbinary(AST_WHILESTMT, cond, body);
+}
+
+/*
+ * Parse a do statement.
+ *
+ * do-statement:
+ *   do statement while ( expression ) ;
+ */
+static struct tree *dostmt(struct parser *parser) {
+	struct tree *body, *cond;
+
+	expect(parser, T_DO);
+	body = stmt(parser);
+
+	expect(parser, T_WHILE);
+	expect(parser, T_LPAREN);
+	cond = expr(parser);
+	expect(parser, T_RPAREN);
+	expect(parser, T_SEMI);
+
+	return mkastbinary(AST_DOSTMT, cond, body);
+}
+
+/*
+ * Parse a switch statement.
+ *
+ * switch-statement:
+ *   switch ( expression ) statement
+ */
+static struct tree *switchstmt(sturct parser *parser) {
+	struct tree *value, body;
+
+	expect(parser, T_SWITCH);
+	expect(parser, T_LPAREN);
+	value = expr(parser);
+
+	expect(parser, T_RPAREN);
+	body = stmt(parser);
+
+	return mkastbinary(AST_SWITCHSTMT, value, body);
+}
+
+/*
+ * Parse an iteration satement.
+ *
+ * iteration-statement:
+ *   while ( expression ) statement
+ *   do statement while ( expression ) ;
+ *   for ( expression-statement expression-statement ) statement
+ *   for ( expression-statement expression-statement expression-statement ) statement
+ *   for ( declaration expression-statement ) statement
+ *   for ( declaration expression-statement expression-statement ) statement
+ */
+static struct tree *iterstmt(struct parser *parser) {
+	switch (parser->token->kind) {
+	case T_WHILE:
+		return whilestmt(parser);
+	case T_DO:
+		return dostmt(parser);
+	case T_FOR:
+		return forstmt(parser);
+	}
+}
+
+/*
+ * Parse a selection statement.
+ *
+ * selection-statement:
+ *   if ( expression ) statement
+ *   if ( expression ) statement else statement
+ *   switch ( expression ) statement
+ */
+static struct tree *selstmt(struct parser *parser) {
+	switch (parser->token->kind) {
+	case T_IF:
+		return ifstmt(parser);
+	case T_SWITCH:
+		return switchstmt(parser);
+	}
+}
+
+/*
+ * Parse a jump statement.
+ *
+ * jump-statement:
+ *   goto identifier ;
+ *   continue ;
+ *   break ;
+ *   return ;
+ *   return expression ;
+ */
+static struct tree *jumpstmt(struct parser *parser) {
+	switch (parser->token->kind) {
+	case T_GOTO:
+		return gotostmt(parser);
+	case T_CONTINUE:
+		return contstmt(parser);
+	case T_BREAK:
+		return breakstmt(parser);
+	case T_RETURN:
+		return returnstmt(parser);
+	}
+}
+
